32d8e86ed37815a455417cc9b006de182b32a33f
lxstnxmad <osnovnoygta@gmail.com>
2023-09-22 21:04:40 +0600

    Update class1.js

--- class1.js
+++ class1.js
@@ -1,43 +1,34 @@
-let userSelected = false;
-
+// Function to load and apply the square state from localStorage
 function loadSquareState(index) {
     const square = document.getElementById(`square${index}`);
     const storedColor = localStorage.getItem(`squareColor${index}`);
-    const storedUsername = localStorage.getItem(`username${index}`);
     if (storedColor) {
         square.style.backgroundColor = storedColor;
     }
+
+    // Load and display the saved username
+    const storedUsername = localStorage.getItem(`username${index}`);
+    const inputForm = document.getElementById(`inputForm${index}`);
     if (storedUsername) {
         const usernameInput = document.getElementById(`usernameInput${index}`);
         usernameInput.value = storedUsername;
+        inputForm.style.display = "none"; // Hide the input form since the username is already set
+    } else {
+        inputForm.style.display = "block"; // Show the input form if no username is saved
     }
 }
 
-
-// Function to save the square state to localStorage
-function saveSquareState(index, color) {
-    localStorage.setItem(`squareColor${index}`, color);
-}
-
-// Function to show the form and handle user input
-function showForm(index) {
-    if (!userSelected) {
-        userSelected = true;
-        const inputForm = document.getElementById(`inputForm${index}`);
-        inputForm.style.display = "block";
-    }
-}
-
+// Function to submit the username
 function submitUsername(index) {
     const usernameInput = document.getElementById(`usernameInput${index}`);
     const square = document.getElementById(`square${index}`);
-    const submitButton = document.querySelector(`#square${index} button[type="submit"]`);
     const username = usernameInput.value;
     if (username.trim() !== "") {
         square.style.backgroundColor = "#921bd4";
         saveSquareState(index, "#921bd4"); // Save the color to localStorage
         localStorage.setItem(`username${index}`, username); // Save the username
         usernameInput.disabled = true;
+        const submitButton = document.querySelector(`#square${index} button`);
         submitButton.style.display = "none";
         alert(`Username ${username} submitted successfully.`);
     } else {
@@ -45,11 +36,11 @@ function submitUsername(index) {
     }
 }
 
-
 // Load the initial square state
-for (let index = 1; index <= 3; index++) {
+for (let index = 1; index <= 26; index++) {
     loadSquareState(index);
 }
+
 
 
 
