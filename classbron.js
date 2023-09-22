79cfedd3cc46af011fb0920f9f3ad1151731a30d
lxstnxmad <osnovnoygta@gmail.com>
2023-09-22 20:41:27 +0600

    updated

--- classbron.js
+++ classbron.js
@@ -18,3 +18,27 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+
+const contentDiv = document.getElementById('content');
+
+        function loadPage(page) {
+            fetch(page + '.html')
+                .then(response => response.text())
+                .then(html => {
+                    contentDiv.innerHTML = html;
+                });
+        }
+
+        // Handle navigation
+        document.querySelectorAll('a').forEach(link => {
+            link.addEventListener('click', (event) => {
+                event.preventDefault();
+                const href = link.getAttribute('href');
+                history.pushState({}, '', href);
+                loadPage(href);
+            });
+        });
+
+        // Initial page load
+        loadPage(location.pathname);
